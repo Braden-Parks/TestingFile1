acef8545748d8a304935ea6c0dca65a245fc1ff2
Braden-Parks <ripper742@gmail.com>
2018-10-10 19:03:33 -0600

    inital send

---
+++ Send to git/mess around.py
@@ -0,0 +1,12 @@
+if __name__ =='__main__':
+    print("i got it working")
+    print("woop woop")
+
+#hello this is my comment
+
+phonenumber = {
+    "John" : 555441,
+    "Jingle" : 7803099,
+    "HimerSmit" : 911
+}
+print(phonenumber)
